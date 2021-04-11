77a3d80af499fb3b70e404b251ac9bf193ded371
Kelton <kelton@orth.org>
2021-04-10 23:10:25 -0700

    Added database integration and signup login authentication

--- index.js
+++ index.js
@@ -12,17 +12,17 @@
 
 const express = require('express');
 const finnhub = require('finnhub');
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
-
 
 //Postgres connestion
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    connectionString: 'postgres://fqlvkimjhnizkt:6cf9bfb4cf165201f6a3fea5e2d2529a3454af2a51dca35d97914efbc05fff5f@ec2-34-198-31-223.compute-1.amazonaws.com:5432/da9r9idh01ouos',
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 client.connect();
@@ -40,7 +40,7 @@ function dropTable(){
 
 function createTable(){
 
-    client.query('CREATE TABLE IF NOT EXISTS "Users" ("userid" VARCHAR, "password" VARCHAR, "holdings" VARCHAR, "balance" INTEGER)', (err, res) => {
+    client.query('CREATE TABLE IF NOT EXISTS "Users" ("username" VARCHAR, "password" VARCHAR, "holdings" VARCHAR, "balance" INTEGER)', (err, res) => {
         if (err) {
             console.log(err.stack);
         } else {
@@ -52,7 +52,7 @@ function createTable(){
 function insertRow(username, password, holdings, balance){
     
     var queryString = 'INSERT INTO "Users" VALUES (\'' + username + '\',\'' + password + '\',\'' + holdings + '\',\'' + balance + '\')';
-    
+
     console.log(queryString);
 
     client.query(queryString, (err, res) => {
@@ -66,9 +66,9 @@ function insertRow(username, password, holdings, balance){
 
 
 function displayAll(){
-    
+
     console.log("Displaying table")
-    
+
     var result = client.query('SELECT * FROM "Users"', (err, res) => {
         if (err) {
             console.log(err.stack);
@@ -76,13 +76,46 @@ function displayAll(){
             console.log(res.rows);
         }
     });
-    
+
     return result;
 }
 
-dropTable();
+
+//dropTable();
 createTable();
 
+
+async function selectFrom(data, table, condition) {
+
+    console.log(`SELECT ${data} FROM ${table} ${condition}`);
+
+  try {
+    const res = await client.query(
+      `SELECT ${data} FROM ${table} ${condition}`
+    );
+    return res.rows;
+  } catch (err) {
+    return err.stack;
+  }
+}
+
+
+async function isCorrectPassword (uname, triedPwd){
+
+    var result = await selectFrom('*', '\"Users\"', 'WHERE username = \'' + uname + '\'');
+    if (result[0].password == triedPwd){
+        console.log("CORRECT");
+        return true;
+    } else {
+        console.log("INCORRECT");
+        console.log(result[0].password);
+        return false;
+    }
+
+}
+
+
+displayAll();
 
 // Set up Finnhub connection
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
@@ -99,6 +132,9 @@ app.engine('hbs', handlebars({
     extname: 'hbs'
 }));
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 // Loads login.hbs inside index.hbs
@@ -106,15 +142,46 @@ app.get('/', (req, res) => {
     res.status(200).render('login', {layout: 'index'})
 });
 
+app.post('/authenticate', async (req, res) => {
+    var usernameProvided = req.body.username;
+    var passwordProvided = req.body.password;
+
+    const auth = await isCorrectPassword(usernameProvided, passwordProvided);
+
+    if (auth == true){
+        console.log("Login Success");
+        res.redirect("/home:" + req.body.username);
+    } else {
+        console.log("Logain Failed");
+        res.send("Login fail");
+    }
+
+});
+
 // Page after logging in
-app.get('/home', (req, res) => {
+app.get('/home:username', (req, res) => {
     res.status(200).render('home');
 });
 
-app.get('/chart', (req, res) => {
-    res.status(200).render('chart');
+app.post('/insertRecord', (req, res) => {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    insertRow(req.body.username, req.body.password, null, 10000);
+    displayAll();
+
+    res.redirect("/home:" + req.body.username);
 });
 
+app.get('/leaderboard', (req, res) => {
+
+    client.query('SELECT * FROM "Users"', (err, response) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            res.send(response.rows);
+        }
+    });
+});
 
 // signup
 app.get('/signup', (req, res) => {
@@ -122,12 +189,11 @@ app.get('/signup', (req, res) => {
 });
 
 
-
 // Endpoints for serving Finnhub data to client
 app.get('/finnhub/candlestick', (req, res) => {
     finnhubClient.stockCandles(req.query.symbol, req.query.interval, req.query.from, req.query.to, {}, (error, data, response) => {
-        res.send(data)
-    })
+            res.send(data)
+      })
 });
 
 app.get('/finnhub/crypto', (req, res) => {
@@ -143,4 +209,4 @@ app.get('/finnhub/quote', (req, res) => {
 });
 
 app.listen(port, () => 
-    console.log(`Express running on port ${port}`));
+           console.log(`Express running on port ${port}`));
